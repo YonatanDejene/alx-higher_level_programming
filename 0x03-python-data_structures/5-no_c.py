f0b29a73f9fe23b1d5f367cb89acc40cabe35e8e
YonatanDejene <33670253+YonatanDejene@users.noreply.github.com>
2023-08-14 10:47:19 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,5 +4,5 @@
 def no_c(my_string):
     # Remove C and c from the list
     replica = [x for x in my_string 
-									if x != 'c' and x != 'C']
+		if x != 'c' and x != 'C']
     return ("".join(replica))
