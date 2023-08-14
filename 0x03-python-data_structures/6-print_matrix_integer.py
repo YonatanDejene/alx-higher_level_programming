9e994825b23a542d7437e5a7ab0dc147088f95f1
YonatanDejene <33670253+YonatanDejene@users.noreply.github.com>
2023-08-14 11:00:33 +0300

    Create 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Task 6-print_matrix_integer.py
+def print_matrix_integer(matrix=[[]]):
+    # Prints a matrix
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=", " 
+                  if col != row[-1] else "")
+        print()
