4d8db52cf5ad17cde750f59ba20aea2803d59ddb
YonatanDejene <33670253+YonatanDejene@users.noreply.github.com>
2023-08-14 11:22:15 +0300

    Create 10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Task 10-divisible_by_2.py
+
+def divisible_by_2(my_list=[]):
+    # Find all multiples of 2 in a list.
+    mult = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            mult.append(True)
+        else:
+            mult.append(False)
+
+    return (mult)
