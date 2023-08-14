2723e7e4c28ddaa00f5ec15aec69d228786598ac
YonatanDejene <33670253+YonatanDejene@users.noreply.github.com>
2023-08-14 11:09:02 +0300

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# 1. Secure access to an element in a list
-
+# Task 1. Secure access to an element in a list
 
 def element_at(my_list, idx):
 	# Get an element form a specific index
