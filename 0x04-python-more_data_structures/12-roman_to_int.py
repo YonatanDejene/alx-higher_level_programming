9d91c86219d86aa2614eb5a2d25cf798834dfdd9
YonatanDejene <33670253+YonatanDejene@users.noreply.github.com>
2023-08-18 10:30:09 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,40 +1,13 @@
 #!/usr/bin/python3
-def to_subtract(list_num):
-    toSub = 0
-    maxList = max(list_num)
-
-    for i in list_num:
-        if maxList > i:
-            toSub += i
-
-    return (maxList - toSub)
-
-
 def roman_to_int(roman_string):
-    if not roman_string:
+    if not roman_string or type(roman_string) != str:
         return 0
-
-    if not isinstance(roman_string, str):
-        return 0
-
-    romNum = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    listKeys = list(romNum.keys())
-
-    num = 0
-    lastRom = 0
-    listNum = [0]
-
-    for c in roman_string:
-        for rNum in list_keys:
-            if rNum == c:
-                if romNum.get(c) <= lastRom:
-                    num += to_subtract(listNum)
-                    listNum = [romNum.get(c)]
-                else:
-                    listNum.append(romNum.get(c))
-
-                lastRom = romNum.get(c)
-
-    num += to_subtract(listNum)
-
-    return (num)
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_n = 0
+    for j in range(len(roman_string)):
+        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
+            roman_n += roman_d[roman_string[j]] - 2 * \
+                        roman_d[roman_string[j - 1]]
+        else:
+            roman_n += roman_d[roman_string[j]]
+    return roman_n
