bf86ea31d4400931036a77843ce520992169a073
yonatandejene <yonatan.dejene@gmail.com>
2023-09-11 16:11:44 +0300

    all tasks

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''Defines class Student.'''
+
+
+class Student:
+    '''Represents student.'''
+
+    def __init__(self, first_name, last_name, age):
+        '''Initializes new Student.
+
+        Args:
+            first_name (str): First name of the student.
+            last_name (str): Last name of the student.
+            age (int): Age of the student.
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Gets dictionary representation of the Student.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        '''
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        '''Replaces all attributes of the Student.
+
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
+        '''
+        for k, v in json.items():
+            setattr(self, k, v)
