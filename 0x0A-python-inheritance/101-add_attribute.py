43765083e76c768956a7a89b2a27487e56c2cb3c
yonatandejene <yonatan.dejene@gmail.com>
2023-09-11 15:45:46 +0300

    adv

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,9 +6,9 @@ def add_attribute(obj, att, value):
     '''Adds new attribute to an object if possible.
 
     Args:
-        value (any): The value of att.
         obj (any): The object to add an attribute to.
         att (str): The name of the attribute to add to obj.
+        value (any): The value of att.
     Raises:
         TypeError: If the attribute cannot be added.
     '''
